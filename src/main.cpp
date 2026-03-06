42541c70eb45423485f0a32efd149e11f047103f
iammasterbrucewayne <batman@dotcodeschool.com>
2026-03-06 20:23:47 +0530

    perf: replace cout with printf to reduce bloat

--- src/main.cpp
+++ src/main.cpp
@@ -1,35 +1,32 @@
-#include <fstream>
-#include <iostream>
-#include <string>
+#include <cstdio>
 #include <string_view>
 
 int main(int argc, char *argv[]) {
   if (argc < 2) {
-    std::cout << "\n  [\033[1;31mERROR\033[0m]: ";
-    std::cout << "No filepath received, exiting program.\n\n";
-    std::cout << "  \033[1mUsage:\033[0m ";
-    std::cout << "decode8086 <binary filepath>\n\n";
+    printf("\n  [\033[1;31mERROR\033[0m]: ");
+    printf("No filepath received, exiting program.\n\n");
+    printf("  \033[1mUsage:\033[0m ");
+    printf("decode8086 <binary filepath>\n\n");
 
     return 1;
   }
 
   if (argc == 2 && std::string_view(argv[1]) == "--help") {
-    std::cout << "\n  \033[1m\033[4m8086 Decoder\033[0m\n\n";
-    std::cout << "  \033[1mUsage:\033[0m ";
-    std::cout << "decode8086 <binary filepath>\n\n";
+    printf("\n  \033[1m\033[4m8086 Decoder\033[0m\n\n");
+    printf("  \033[1mUsage:\033[0m ");
+    printf("decode8086 <binary filepath>\n\n");
 
     return 0;
   }
 
   if (argc > 2) {
-    std::cout << "\n  [\033[1;31mERROR\033[0m]: ";
-    std::cout
-        << "The decoder currently only supports one filepath at a time.\n\n";
+    printf("\n  [\033[1;31mERROR\033[0m]: ");
+    printf("The decoder currently only supports one filepath at a time.\n\n");
 
     return 1;
   }
 
-  std::cout << argc << ", " << argv[1] << "\n";
+  printf("%i, %s\n", argc, argv[1]);
 
   return 0;
 }
